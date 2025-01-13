5d3d2bbad4b091ab7d02991cfeecb01f5b059fc8
Akinola Ayobami <ayobamiakinola84@gmail.com>
2025-01-13 19:13:45 +0100

    feat: ayobami

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "ayobamiakinola84@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
